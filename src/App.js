986993b90bdcd20c5149be0ed87ef92559f0ad19
Jannes Coetzee <jannesrsa@gmail.com>
2021-05-22 19:11:05 +0200

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import Header from "./Header";
 import Home from "./Home";
+import Welcome from "./Welcome";
+import Navigation from "./Navigation";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   const [user, setUser] = useState("Ray");
 
   return (
-    <>
-      <Header onChangeUser={setUser} />
-      <Home user={user} />;
-    </>
+    <Router>
+      <>
+        {user && <Welcome user={user} />}
+        <Navigation user={user} />
+        <Route path="/" exact render={(props) => <Home user={user} />}></Route>
+      </>
+    </Router>
   );
 };
 
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+const Welcome = ({ user }) => {
+  return (
+    <div className="text-center mt-4">
+      <span className="text-secondary font-weight-bold pl-1">Welcome {user}</span>,
+      <a href="/" className="font-weight-bold text-primary pl-1">
+        log out
+      </a>
+    </div>
+  );
+};
+
+export default Welcome;
