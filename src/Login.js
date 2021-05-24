a20a1d1a8ac91de21a13f223ccb4104af6ed4e8d
Jannes Coetzee <jannesrsa@gmail.com>
2021-05-24 09:24:04 +0200

    Style login

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import FormError from "./FormError";
 import firebase from "./Firebase";
 import { useHistory } from "react-router-dom";
 
-const Login = ({ user, setUser }) => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -28,46 +28,51 @@ const Login = ({ user, setUser }) => {
   };
 
   return (
-    <div className="container" onSubmit={onSubmit}>
-      <form>
-        <div class="form-group">
-          {errorMessage && <FormError theMessage={errorMessage} />}
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
+    <form className="mt-3" onSubmit={onSubmit}>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            <div className="card bg-light">
+              <div className="card-body">
+                <h3 className="font-weight-light mb-3">Log in</h3>
+                <section className="form-group">
+                  {errorMessage && <FormError theMessage={errorMessage} />}
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <small id="emailHelp" class="form-text text-muted">
+                    We'll never share your email with anyone else.
+                  </small>
+                </section>
+                <section className="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </section>
+                <div className="form-group text-right mb-0">
+                  <button className="btn btn-primary" type="submit">
+                    Log in
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        {/* <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div> */}
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+      </div>
+    </form>
   );
 };
 
